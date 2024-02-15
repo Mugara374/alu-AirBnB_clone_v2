79e5cef689151b56de30cfb7b6491bf21d0876c8
Mugara374 <m.mushi@alustudent.com>
2024-02-15 15:36:59 +0200

    setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,41 +1,27 @@
 #!/usr/bin/env bash
 # script that sets up web servers for the deployment of web_static
-# Update package lists
 sudo apt-get update
-
-# Install Nginx
 sudo apt-get -y install nginx
-
-# Allow HTTP traffic through the firewall
 sudo ufw allow 'Nginx HTTP'
 
-# Create necessary directories
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/current/
-
-# Create a fake HTML file
-sudo tee /data/web_static/releases/test/index.html > /dev/null <<EOF
-<html>
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>
-EOF
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Create symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-# Set ownership to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-# Restart Nginx
 sudo service nginx restart
-
-# Exit with success
-exit 0
