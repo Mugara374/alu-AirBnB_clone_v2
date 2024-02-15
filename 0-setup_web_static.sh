69fb1f8e348c2e1adf4eb0011c44ebbe3eb311a1
Mugara374 <m.mushi@alustudent.com>
2024-02-15 15:52:08 +0200

    setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,29 +1,37 @@
 #!/usr/bin/env bash
 # script that sets up web servers for the deployment of web_static
+
+# Update package lists
 sudo apt-get update
+
+# Install Nginx
 sudo apt-get -y install nginx
+
+# Allow HTTP traffic through the firewall
 sudo ufw allow 'Nginx HTTP'
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
+# Create necessary directories and files with sudo privileges using a here document
+sudo bash <<EOF
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/current/
+touch /data/web_static/releases/test/index.html
+echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+</html>" | tee /data/web_static/releases/test/index.html > /dev/null
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+EOF
 
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
-
-sudo chown -R ubuntu:ubuntu /data/
-
+# Update Nginx configuration
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
+# Restart Nginx
 sudo service nginx restart
 
+# Exit with success
 exit 0
