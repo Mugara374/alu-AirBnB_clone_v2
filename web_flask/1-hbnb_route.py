ecb78bb98a45a4f4e963f096921c33d3967da69e
Mugara374 <m.mushi@alustudent.com>
2024-02-24 17:49:27 +0000

    tasks done

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
-""" Script that runs an app with Flask framework """
+"""
+Write a script that starts a Flask web application:
+Your web application must be listening on 0.0.0.0, port 5000
+Routes:
+/: display Hello HBNB!
+/hbnb: display HBNB
+You must use the option strict_slashes=False in your route definition
+"""
+
+
 from flask import Flask
 
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ Function called with / route """
-    return 'Hello HBNB!'
+@app.route("/", strict_slashes=False)
+def root_display():
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """ Function called with /hbnb route """
-    return 'HBNB'
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_display():
+    return "HBNB"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    # specify IP address and port number
+    app.run(host="0.0.0.0", port=5000)
--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,42 +1,47 @@
 #!/usr/bin/python3
-""" Script that runs an app with Flask framework """
-from flask import Flask
+"""
+Write a script that starts a Flask web application:
+Your web application must be listening on 0.0.0.0, port 5000
+Routes:
+/: display Hello HBNB!
+/hbnb: display HBNB
+You must use the option strict_slashes=False in your route definition
+"""
+
+
+from flask import Flask, url_for
 
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ Function called with / route """
-    return 'Hello HBNB!'
+@app.route("/", strict_slashes=False)
+def root_display():
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """ Function called with /hbnb route """
-    return 'HBNB'
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_display():
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """ Function called with /c/<text> route """
-    return 'C %s' % text.replace('_', ' ')
+@app.route("/c/<text>", strict_slashes=False)
+def c_display(text):
+    text_str = " ".join(text.split("_"))
+    return "C {0}".format(text_str)
 
 
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_text(text='is cool'):
-    """ Function called with /python/<text> route """
-    if text is not 'is cool':
-        text = text.replace('_', ' ')
-    return 'Python %s' % text
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_display(text="is cool"):
+    text_str = " ".join(text.split("_"))
+    return "Python {0}".format(text_str)
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """ Function called with /number/<n> route """
-    return "%d is a number" % n
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_display(n):
+    return "{0} is a number".format(n)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    # specify IP address and port number
+    app.run(host="0.0.0.0", port=5000)
